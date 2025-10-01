83f8caf3e9136038fb06a26d21ef84f457590478
MP4ZA <65838086+MP4ZA@users.noreply.github.com>
2025-10-01 13:37:07 +0800

    FA

--- HW1/FA.v
+++ HW1/FA.v
@@ -6,9 +6,25 @@ module FA(
 	output  c_out
 );
 
-/*
-	Write Your Design Here ~
-*/
+	wire sum_1;
+	wire carry_1;
+	wire carry_2;
+
+	HA ha1(
+		.x(x),
+		.y(y),
+		.s(sum_1),
+		.c(carry_1)
+	);
+
+	HA ha2(
+		.x(c_in),
+		.y(sum_1),
+		.s(s),
+		.c(carry_2)
+	);
+
+	assign c_out = carry_1 | carry_2;
 
 endmodule
 
