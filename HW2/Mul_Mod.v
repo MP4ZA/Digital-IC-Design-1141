9e7f1ab9b6b2e6cef77f675032d9262477f832b4
MP4ZA <65838086+MP4ZA@users.noreply.github.com>
2025-10-02 14:56:46 +0800

    hw1 rca number & hw2 adder locate

--- HW2/Mul_Mod.v
+++ HW2/Mul_Mod.v
@@ -3,25 +3,38 @@ module Mul_Mod (
     input  [22:0] B,
     output [23:0] Z
 );
-
-    wire [28:0] mult1 = A * B[22:17];
+    wire [28:0] pre_mult1 = (A * B[22:17]);
+    wire [45:0] mult1 = pre_mult1  << 17;
     wire [39:0] mult2 = A * B[16:0];
-    wire [45:0] U = (mult1 << 17) + mult2;
+    
+    // // //
+    wire [45:0] U = mult1 + mult2;
+    // // //
 
     wire [23:0] Y = U[23:0];
 
     wire [23:0] V = U[45:22];
     wire [24:0] row1;
     wire [9:0]  row2;
+
+    // // //
     assign row1 = V + V[23:10];
+    // // //
+
     assign row2 = V[9:0];
 
     wire [34:0] big1 = {row1, row2};
 
+    // // //
     wire [25:0] pre_big2 = (V << 1) + V + V[23:1];
+    // // //
+
     wire [13:0] big2 = pre_big2[25:12];
 
+    // // //
     wire [34:0] pre_W = big1 + big2;
+    // // //
+    
     // W 分開
     wire [23:0] W = pre_W[34:11];
     wire [10:0] sub1 = W[23:13] - W[10:0];
